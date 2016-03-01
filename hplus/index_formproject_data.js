f33e94fb84ff701b8a2e07116f8509beb2db5314
hao.zhang@neusoft.com <seannell@outlook.com>
2016-03-01 20:32:36 +0800

    根据JSON数据生成填报大厅的数据

---
+++ hplus/index_formproject_data.js
@@ -0,0 +1,28 @@
+/**
+ * Created by zhanghao on 2016/3/1.
+ */
+// Task data
+var taskList = {result:[
+    {id:"task1",name:"教师职业资格申请",url:"#",dualtime:"2016年12月1日",publishtime:"2015年12月1日",status:"1",class:"我是类型",contactPerson:"赵小红",contactNumber:"18698666325",commitNumber:"133"},
+    {id:"task2",name:"教师职业资格申请",url:"#",dualtime:"2016年12月1日",publishtime:"2015年12月1日",status:"1",class:"我是类型",contactPerson:"赵小红",contactNumber:"18698666325",commitNumber:"133"},
+    {id:"task3",name:"教师职业资格申请",url:"#",dualtime:"2016年12月1日",publishtime:"2015年12月1日",status:"1",class:"我是类型",contactPerson:"赵小红",contactNumber:"18698666325",commitNumber:"133"},
+    {id:"task4",name:"教师职业资格申请",url:"#",dualtime:"2016年12月1日",publishtime:"2015年12月1日",status:"1",class:"我是类型",contactPerson:"赵小红",contactNumber:"18698666325",commitNumber:"133"},
+    {id:"task5",name:"教师职业资格申请",url:"#",dualtime:"2016年12月1日",publishtime:"2015年12月1日",status:"0",class:"我是类型",contactPerson:"赵小红",contactNumber:"18698666325",commitNumber:"133"},
+    {id:"task6",name:"教师职业资格申请",url:"#",dualtime:"2016年12月1日",publishtime:"2015年12月1日",status:"1",class:"我是类型",contactPerson:"赵小红",contactNumber:"18698666325",commitNumber:"133"},
+    {id:"task7",name:"教师职业资格申请",url:"#",dualtime:"2016年12月1日",publishtime:"2015年12月1日",status:"1",class:"我是类型",contactPerson:"赵小红",contactNumber:"18698666325",commitNumber:"133"},
+    {id:"task8",name:"教师职业资格申请",url:"#",dualtime:"2016年12月1日",publishtime:"2015年12月1日",status:"1",class:"我是类型",contactPerson:"赵小红",contactNumber:"18698666325",commitNumber:"133"},
+    {id:"task9",name:"教师职业资格申请",url:"#",dualtime:"2016年12月1日",publishtime:"2015年12月1日",status:"1",class:"我是类型",contactPerson:"赵小红",contactNumber:"18698666325",commitNumber:"133"}
+]};
+
+// Service DATA
+var serviceList = {result:[
+    {id:"service1",name:"请假申请",url:"#",icon:"fa-user-times",color:"danger",status:"1",type:""},
+    {id:"service2",name:"图书预约",url:"#",icon:"fa-book",color:"warning",status:"1"},
+    {id:"service3",name:"档案预约",url:"#",icon:"fa-archive",color:"info",status:"1"},
+    {id:"service4",name:"网络开通",url:"#",icon:"fa-internet-explorer",color:"info",status:"1"},
+    {id:"service5",name:"一卡通补卡",url:"#",icon:"fa-credit-card",color:"default",status:"0"},
+    {id:"service6",name:"上机位申请",url:"#",icon:"fa-tv",color:"info",status:"1"},
+    {id:"service7",name:"教室预订",url:"#",icon:"fa-map-marker",color:"info",status:"1"},
+    {id:"service8",name:"演讲厅预订",url:"#",icon:"fa-university",color:"info",status:"1"},
+    {id:"service9",name:"研究生包厢申请",url:"#",icon:"fa-mortar-board",color:"info",status:"1"}
+]};
